6024707d35ead44b19842d29bc44ba991a7d9a79
Joshyybaxx <josh@compdemo.net>
2025-05-23 14:55:48 +1000

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import LeadGenROICalculator from "../components/LeadGenROICalculator";
-
-export default function Home() {
-  return (
-    <main className="bg-black min-h-screen">
-      <LeadGenROICalculator />
-    </main>
-  );
-}
