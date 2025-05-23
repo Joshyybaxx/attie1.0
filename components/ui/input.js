47ed62a58d56e3245fdac5fe2bd241488474ebbd
Joshyybaxx <josh@compdemo.net>
2025-05-23 14:55:38 +1000

    Delete components directory

--- components/ui/input.js
+++
@@ -1,3 +0,0 @@
-export function Input({ type = 'text', name, value, onChange, className }) {
-  return <input type={type} name={name} value={value} onChange={onChange} className={`border rounded px-2 py-1 w-full ${className}`} />;
-}
